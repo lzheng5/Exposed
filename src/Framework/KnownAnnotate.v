8b57c8459d7819953b8fc024d21845717a62bc6e
lzheng5 <lingxiaozheng515@gmail.com>
2026-02-12 07:40:16 -0500

    snapshot

--- src/Framework/KnownAnnotate.v
+++ src/Framework/KnownAnnotate.v
@@ -1361,8 +1361,8 @@ Module M <: Annotate.
   End Known.
 
   Module Top.
-    (* Top-level Logical Relations *)
 
+    (* Top-level Logical Relations *)
     Fixpoint V_top (i : nat) (v1 : A0.val) (wv2 : A1.wval) {struct i} : Prop :=
       wf_val wv2 /\
       exposed wv2 /\
@@ -1513,6 +1513,13 @@ Module M <: Annotate.
           exposed v2 /\
           V_top i v1 v2.
 
+    Lemma G_top_K i Γ1 ρ1 Γ2 ρ2 :
+      G_top i Γ1 ρ1 Γ2 ρ2 ->
+      forall x,
+        (x \in Γ1) ->
+        (forall (K : known_map), K ! x = None).
+    Proof. unfold G_top; fcrush. Qed.
+
     Lemma G_top_wf_env_r i Γ1 ρ1 Γ2 ρ2 :
       G_top i Γ1 ρ1 Γ2 ρ2 ->
       wf_env ρ2.
@@ -1533,6 +1540,65 @@ Module M <: Annotate.
       intros.
       destruct H as [Hr [Hs HG]].
       repeat (split; auto).
+    Qed.
+
+    (* Top-level Lemmas about [wf_val], [wf_res], and [wf_env] *)
+    Lemma V_top_wf_val_r {i v1 v2}:
+      V_top i v1 v2 ->
+      wf_val v2.
+    Proof.
+      intros HV.
+      destruct i; simpl in *;
+        destruct HV as [Hv2 _]; auto.
+    Qed.
+
+    Lemma V_top_wf_val_Forall_r {i vs1 vs2} :
+      Forall2 (V_top i) vs1 vs2 ->
+      Forall wf_val vs2.
+    Proof.
+      intros.
+      induction H; auto.
+      constructor; auto.
+      eapply V_top_wf_val_r; eauto.
+    Qed.
+
+    Lemma V_top_wf_res_r {i v1 v2}:
+      V_top i v1 v2 ->
+      wf_res (Res v2).
+    Proof.
+      intros HV.
+      constructor.
+      eapply V_top_wf_val_r; eauto.
+    Qed.
+
+    Lemma V_top_exposed_r {i v1 v2}:
+      V_top i v1 v2 ->
+      exposed v2.
+    Proof.
+      intros.
+      destruct i; destruct v2;
+        simpl in *; fcrush.
+    Qed.
+
+    Lemma R_top_wf_res_l {i r1 r2} :
+      R_top i r1 r2 ->
+      wf_res r2.
+    Proof.
+      unfold R.
+      intros.
+      destruct r1; destruct r2; try contradiction; auto.
+      constructor.
+      eapply V_top_wf_val_r; eauto.
+    Qed.
+
+    (* Top-level Inversion Lemmas *)
+    Lemma R_top_res_inv_l i v1 r2 :
+      R_top i (A0.Res v1) r2 ->
+      exists v2, r2 = A1.Res v2 /\ V_top i v1 v2.
+    Proof.
+      intros.
+      destruct r2; simpl in *; try contradiction.
+      eexists; split; eauto.
     Qed.
 
     (* Top-level Monotonicity Lemmas *)
@@ -1860,8 +1926,8 @@ Module M <: Annotate.
         exists v1 v2,
           M.get x ρ1 = Some v1 /\
           M.get x ρ2 = Some v2 /\
-          binding_inv_top x v2 /\
-          V i v1 v2.
+          exposed v2 /\
+          V_top i v1 v2.
     Proof.
       unfold G.
       intros.
@@ -1873,13 +1939,11 @@ Module M <: Annotate.
       G_top i Γ1 ρ1 Γ2 ρ2 ->
       forall xs,
         (FromList xs) \subset Γ1 ->
-        Disjoint _ (FromList xs) (Dom_map K) ->
         exists vs1 vs2,
           get_list xs ρ1 = Some vs1 /\
           get_list xs ρ2 = Some vs2 /\
           Forall exposed vs2 /\
-          Forall2 binding_inv_top xs vs2 /\
-          Forall2 (V i) vs1 vs2.
+          Forall2 (V_top i) vs1 vs2.
     Proof.
       intros HG xs.
       intros.
@@ -1887,9 +1951,8 @@ Module M <: Annotate.
       - eexists; eexists; repeat split; eauto.
       - rewrite FromList_cons in H.
         edestruct (G_top_get HG) as [v1 [v2 [Heqv1 [Heqv2 [Hb HV]]]]]; eauto.
-        eapply Disjoint_FromList_cons_inv in H0; inv H0; auto.
 
-        edestruct IHxs as [vs1 [vs2 [Heqvs1 [Heqvs2 [Hexs [Hbs HVs]]]]]]; eauto.
+        edestruct IHxs as [vs1 [vs2 [Heqvs1 [Heqvs2 [Hexs HVs]]]]]; eauto.
         eapply Included_trans; eauto.
         apply Included_Union_r.
 
@@ -1898,16 +1961,14 @@ Module M <: Annotate.
         rewrite Heqv2.
         rewrite Heqvs2.
         exists (v1 :: vs1), (v2 :: vs2); repeat (split; auto).
-        constructor; auto.
-        inv Hb.
-        inv H3; auto.
     Qed.
 
     Lemma G_top_set {i Γ1 ρ1 Γ2 ρ2}:
       G_top i Γ1 ρ1 Γ2 ρ2 ->
       forall {x v1 v2},
-        binding_inv_top x v2 ->
-        V i v1 v2 ->
+        exposed v2 ->
+        V_top i v1 v2 ->
+        (forall (K : known_map), K ! x = None) ->
         G_top i (x |: Γ1) (M.set x v1 ρ1) (x |: Γ2) (M.set x v2 ρ2).
     Proof.
       intros.
@@ -1916,7 +1977,7 @@ Module M <: Annotate.
       split.
       eapply wf_env_set; eauto.
       eapply G_top_wf_env_r; eauto.
-      eapply V_wf_val_r; eauto.
+      eapply V_top_wf_val_r; eauto.
 
       split.
       apply Included_Union_compat; auto.
@@ -1926,21 +1987,22 @@ Module M <: Annotate.
       intros.
       destruct (M.elt_eq x0 x); subst.
       - repeat rewrite M.gss.
-        inv H0.
+        split; auto.
         eexists; eexists; repeat split; eauto.
       - repeat (rewrite M.gso; auto).
+        assert (x0 \in Γ1) by fcrush.
+        split.
+        eapply G_top_K; eauto.
         eapply G_top_get; eauto.
-        inv H2; auto.
-        inv H3; contradiction.
     Qed.
 
     Lemma G_top_set_lists {i Γ1 ρ1 Γ2 ρ2}:
       G_top i Γ1 ρ1 Γ2 ρ2 ->
       forall {xs vs1 vs2 ρ3 ρ4},
-        Forall2 (V i) vs1 vs2 ->
-        Forall2 binding_inv_top xs vs2 ->
+        Forall2 (V_top i) vs1 vs2 ->
         set_lists xs vs1 ρ1 = Some ρ3 ->
         set_lists xs vs2 ρ2 = Some ρ4 ->
+        Forall (fun x => (forall (K : known_map), K ! x = None)) xs ->
         G_top i (FromList xs :|: Γ1) ρ3 (FromList xs :|: Γ2) ρ4.
     Proof.
       intros HG xs.
@@ -1948,7 +2010,7 @@ Module M <: Annotate.
       induction xs; simpl; intros.
       - destruct vs1; try discriminate.
         destruct vs2; try discriminate.
-        inv H1; inv H2.
+        inv H0; inv H1.
         eapply G_top_subset; eauto.
         normalize_sets.
         rewrite Union_Empty_set_neut_l; eauto.
@@ -1962,6 +2024,7 @@ Module M <: Annotate.
         inv H; inv H0; inv H1; inv H2.
         eapply G_top_subset with (Γ1 := (a |: (FromList xs :|: Γ1))) (Γ2 := (a |: (FromList xs :|: Γ2))); eauto.
         eapply G_top_set; eauto.
+        eapply V_top_exposed_r; eauto.
         normalize_sets.
         rewrite Union_assoc.
         apply Included_refl.
@@ -1969,54 +2032,33 @@ Module M <: Annotate.
         apply Included_refl.
     Qed.
 
-    (* Monotonicity Lemma *)
-    Lemma G_top_mono {Γ1 Γ2 ρ1 ρ2} i j:
-      G_top i Γ1 ρ1 Γ2 ρ2 ->
-      j <= i ->
-      G_top j Γ1 ρ1 Γ2 ρ2.
-    Proof.
-      unfold G_top.
-      intros.
-      destruct H as [Hr2 [HS HG]].
-      repeat (split; auto); intros.
-      edestruct HG as [v1 [v2 [Heqv1 [Heqv2 [[Hk Hex] HV]]]]]; eauto.
-      eexists; eexists; repeat split; eauto.
-      apply V_mono with i; eauto.
-    Qed.
-
     (* Compatibility Lemmas *)
     Lemma Vfun_V_top e e' :
-      known_map_inv K ->
       trans_correct_top e e' ->
       forall i f xs Γ1 Γ2 ρ1 ρ2,
-        K ! f = None ->
-        Disjoint _ (FromList xs) (Dom_map K) ->
+        (forall (K : known_map), K ! f = None) ->
 
         G_top i Γ1 ρ1 Γ2 ρ2 ->
         A0.occurs_free e \subset (FromList xs :|: (f |: Γ1)) ->
         A1.occurs_free e' \subset (FromList xs :|: (f |: Γ2)) ->
-        V i (A0.Vfun f ρ1 xs e) (Tag w0 (A1.Vfun f ρ2 xs e')).
+        V_top i (A0.Vfun f ρ1 xs e) (Tag w0 (A1.Vfun f ρ2 xs e')).
     Proof.
       unfold trans_correct_top.
-      intros HK [HS He] i.
+      intros [HS He] i.
       assert (Hw0 : w0 \in Exposed) by (apply w0_exposed).
 
       induction i; simpl; intros; auto;
         assert (Hρ2 : wf_env ρ2) by (eapply G_top_wf_env_r; eauto);
-        repeat (split; auto); intros;
-        destruct (K ! f) eqn:HKf; try discriminate;
-        split; auto; intros.
+        repeat (split; auto); intros.
 
       apply (He (i - (i - j)) ρ3 ρ4); auto.
       eapply G_top_subset with (Γ1 := FromList xs :|: (f |: Γ1)) (Γ2 := FromList xs :|: (f |: Γ2)); eauto.
       eapply G_top_set_lists; eauto.
       eapply G_top_set; eauto.
       eapply G_top_mono; eauto; try lia.
-      eapply binding_inv_top_exposed; simpl; eauto.
-      apply V_mono with i; try lia.
+      apply V_top_mono with i; try lia.
       eapply IHi with (Γ2 := Γ2); eauto.
       apply G_top_mono with (S i); eauto; lia.
-      eapply binding_inv_top_exposed_Forall; eauto.
       eapply set_lists_length_eq; eauto.
     Qed.
 
