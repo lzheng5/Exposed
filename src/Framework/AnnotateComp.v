88c17a5f17abd89643afdd6d2dfefb39e103fc8a
lzheng5 <lingxiaozheng515@gmail.com>
2026-02-14 16:03:34 -0500

    Comp

--- src/Framework/AnnotateComp.v
+++ src/Framework/AnnotateComp.v
@@ -9,163 +9,161 @@ From Framework Require Import Util ANF0 ANF Refl0 Refl Annotate.
 Module A0 := ANF0.
 Module A1 := ANF.
 
+Module AM := AnnotateTop.
+Import AM.
+
 (* Cross-language Compositionality *)
 
-Module AnnotateComp.
+(* Adequacy *)
+Theorem adequacy e1 e2:
+  trans_correct e1 e2 ->
+  forall ρ1 ρ2,
+    wf_env ρ2 ->
+    (forall k, G k (A0.occurs_free e1) ρ1 (A1.occurs_free e2) ρ2) ->
+    forall j1 r1,
+      A0.bstep_fuel ρ1 e1 j1 r1 ->
+      exists j2 r2,
+        A1.bstep_fuel true ρ2 e2 j2 r2 /\
+        (forall k, R k r1 r2).
+Proof.
+  intros.
+  unfold trans_correct in H.
+  destruct H as [HS HT].
 
-  Import AnnotateTop.
+  assert (HE : E true j1 ρ1 e1 ρ2 e2) by (eapply (HT j1); eauto).
+  edestruct (HE j1) as [j2 [r2 [Hstep2 HR]]]; eauto.
+  eexists; eexists; split; eauto.
 
-  (* Adequacy *)
-  Theorem adequacy e1 e2:
-    trans_correct e1 e2 ->
-    forall ρ1 ρ2,
-      wf_env ρ2 ->
-      (forall k, G k (A0.occurs_free e1) ρ1 (A1.occurs_free e2) ρ2) ->
-      forall j1 r1,
-        A0.bstep_fuel ρ1 e1 j1 r1 ->
-        exists j2 r2,
-          A1.bstep_fuel true ρ2 e2 j2 r2 /\
-          (forall k, R k r1 r2).
-  Proof.
-    intros.
-    unfold trans_correct in H.
-    destruct H as [HS HT].
+  intros.
+  assert (HE' : E true (j1 + k) ρ1 e1 ρ2 e2) by (eapply HT; eauto).
+  edestruct (HE' j1) as [j2' [r2' [Hstep2' HR']]]; eauto; try lia.
 
-    assert (HE : E true j1 ρ1 e1 ρ2 e2) by (eapply (HT j1); eauto).
-    edestruct (HE j1) as [j2 [r2 [Hstep2 HR]]]; eauto.
-    eexists; eexists; split; eauto.
+  rewrite_math (j1 + k - j1 = k).
+  rewrite_math (j1 - j1 = 0).
 
-    intros.
-    assert (HE' : E true (j1 + k) ρ1 e1 ρ2 e2) by (eapply HT; eauto).
-    edestruct (HE' j1) as [j2' [r2' [Hstep2' HR']]]; eauto; try lia.
+  destruct r2; destruct r2'; destruct r1;
+    simpl in *; auto; try contradiction.
 
-    rewrite_math (j1 + k - j1 = k).
-    rewrite_math (j1 - j1 = 0).
+  edestruct (A1.bstep_fuel_deterministic w w0 Hstep2 Hstep2'); subst; eauto.
+Qed.
 
-    destruct r2; destruct r2'; destruct r1;
-      simpl in *; auto; try contradiction.
+(* Behavioral Refinement *)
+Inductive val_ref_ : A0.val -> A1.wval -> Prop :=
+| Ref_Vfun :
+  forall f1 ρ1 w xs1 e1 f2 ρ2 xs2 e2,
+    val_ref_ (A0.Vfun f1 ρ1 xs1 e1) (Tag w (A1.Vfun f2 ρ2 xs2 e2))
 
-    edestruct (A1.bstep_fuel_deterministic w w0 Hstep2 Hstep2'); subst; eauto.
-  Qed.
+| Ref_Vconstr_nil :
+  forall w c,
+    val_ref_ (A0.Vconstr c []) (Tag w (A1.Vconstr c []))
 
-  (* Behavioral Refinement *)
-  Inductive val_ref_ : A0.val -> A1.wval -> Prop :=
-  | Ref_Vfun :
-    forall f1 ρ1 w xs1 e1 f2 ρ2 xs2 e2,
-      val_ref_ (A0.Vfun f1 ρ1 xs1 e1) (Tag w (A1.Vfun f2 ρ2 xs2 e2))
+| Ref_Vconstr_cons :
+  forall w c v1 v2 vs1 vs2,
+    val_ref_ v1 v2 ->
+    val_ref_ (A0.Vconstr c vs1) (Tag w (A1.Vconstr c vs2)) ->
+    val_ref_ (A0.Vconstr c (v1 :: vs1)) (Tag w (A1.Vconstr c (v2 :: vs2))).
 
-  | Ref_Vconstr_nil :
-    forall w c,
-      val_ref_ (A0.Vconstr c []) (Tag w (A1.Vconstr c []))
+Hint Constructors val_ref_ : core.
 
-  | Ref_Vconstr_cons :
-    forall w c v1 v2 vs1 vs2,
-      val_ref_ v1 v2 ->
-      val_ref_ (A0.Vconstr c vs1) (Tag w (A1.Vconstr c vs2)) ->
-      val_ref_ (A0.Vconstr c (v1 :: vs1)) (Tag w (A1.Vconstr c (v2 :: vs2))).
+Definition val_ref := val_ref_.
 
-  Hint Constructors val_ref_ : core.
+Hint Unfold val_ref : core.
 
-  Definition val_ref := val_ref_.
+Lemma val_ref_Vconstr c w vs1 vs2 :
+  Forall2 val_ref vs1 vs2 ->
+  val_ref (A0.Vconstr c vs1) (Tag w (A1.Vconstr c vs2)).
+Proof.
+  intros.
+  induction H; simpl; auto.
+Qed.
 
-  Hint Unfold val_ref : core.
+Theorem V_val_ref {v1 v2} :
+  (forall i, V i v1 v2) ->
+  val_ref v1 v2.
+Proof.
+  unfold val_ref.
+  revert v2.
+  induction v1 using val_ind'; intros; simpl.
+  - specialize (H 0).
+    destruct v2.
+    simpl in H.
+    destruct H as [Hwf HV].
+    destruct (exposed_reflect w); inv HV.
+    destruct v; try contradiction.
+    inv H0.
+    destruct l; try discriminate; auto.
+  - destruct v2.
+    pose proof (H 0) as H0; simpl in *.
+    destruct H0 as [Hw HV].
+    destruct (exposed_reflect w); inv HV.
+    destruct v; try contradiction.
+    destruct H1 as [Hc Hlen]; subst.
 
-  Lemma val_ref_Vconstr c w vs1 vs2 :
-    Forall2 val_ref vs1 vs2 ->
-    val_ref (A0.Vconstr c vs1) (Tag w (A1.Vconstr c vs2)).
-  Proof.
-    intros.
-    induction H; simpl; auto.
-  Qed.
+    destruct l0; simpl in *; inv Hlen.
+    inv H0.
 
-  Theorem V_val_ref {v1 v2} :
-    (forall i, V i v1 v2) ->
-    val_ref v1 v2.
-  Proof.
-    unfold val_ref.
-    revert v2.
-    induction v1 using val_ind'; intros; simpl.
-    - specialize (H 0).
-      destruct v2.
-      simpl in H.
-      destruct H as [Hwf HV].
-      destruct (exposed_reflect w); inv HV.
-      destruct v; try contradiction.
-      inv H0.
-      destruct l; try discriminate; auto.
-    - destruct v2.
-      pose proof (H 0) as H0; simpl in *.
-      destruct H0 as [Hw HV].
-      destruct (exposed_reflect w); inv HV.
-      destruct v; try contradiction.
-      destruct H1 as [Hc Hlen]; subst.
+    assert (HV' : forall i, V i v1 t /\ V i (A0.Vconstr c l) (Tag w (A1.Vconstr c l0))).
+    {
+      intros.
+      specialize (H (S i0)); simpl in *.
+      destruct H as [_ HV]; subst.
+      destruct (exposed_reflect w); try contradiction.
+      destruct HV as [Hex [Hc HFV]]; subst; eauto.
 
-      destruct l0; simpl in *; inv Hlen.
-      inv H0.
+      inv HFV.
+      split.
+      eapply V_mono; eauto; lia.
 
-      assert (HV' : forall i, V i v1 t /\ V i (A0.Vconstr c l) (Tag w (A1.Vconstr c l0))).
+      assert (He' : exposed (Tag w (A1.Vconstr c l0))).
       {
-        intros.
-        specialize (H (S i0)); simpl in *.
-        destruct H as [_ HV]; subst.
-        destruct (exposed_reflect w); try contradiction.
-        destruct HV as [Hex [Hc HFV]]; subst; eauto.
-
-        inv HFV.
-        split.
-        eapply V_mono; eauto; lia.
-
-        assert (He' : exposed (Tag w (A1.Vconstr c l0))).
-        {
-          inv Hex; inv H6; auto.
-        }
-
-        assert (Hw' : wf_val (Tag w (A1.Vconstr c l0))).
-        {
-          constructor; intros; auto.
-          inv Hw.
-          inv H5; auto.
-        }
-
-        inv H6.
-        inv Hw.
-        inv H8.
-        destruct i0; unfold V; simpl in *;
-          destruct (exposed_reflect w); try contradiction;
-          repeat (split; auto);
-          simpl in *;
-          rewrite_math (i0 - i0 = 0);
-          rewrite_math (i0 - 0 = i0);
-          try (eapply V_mono_Forall; eauto; lia).
+        inv Hex; inv H6; auto.
       }
 
-      assert (HV0 : forall i, V i v1 t) by (intros i0; destruct (HV' i0); auto).
-      assert (HV1 : forall i, V i (A0.Vconstr c l) (Tag w (A1.Vconstr c l0))) by (intros i0; destruct (HV' i0); auto).
-      auto.
-    - specialize (H 0); simpl in *.
-      destruct H as [Hw HV].
-      destruct v2; try contradiction; auto.
-      destruct (exposed_reflect w); inv HV.
-      destruct v; try contradiction; auto.
-  Qed.
+      assert (Hw' : wf_val (Tag w (A1.Vconstr c l0))).
+      {
+        constructor; intros; auto.
+        inv Hw.
+        inv H5; auto.
+      }
 
-  Corollary R_res_val_ref {v1 v2} :
-    (forall i, R i (A0.Res v1) (A1.Res v2)) ->
-    val_ref v1 v2.
-  Proof. intros; eapply V_val_ref; eauto. Qed.
+      inv H6.
+      inv Hw.
+      inv H8.
+      destruct i0; unfold V; simpl in *;
+        destruct (exposed_reflect w); try contradiction;
+        repeat (split; auto);
+        simpl in *;
+        rewrite_math (i0 - i0 = 0);
+        rewrite_math (i0 - 0 = i0);
+        try (eapply V_mono_Forall; eauto; lia).
+    }
 
-  (* Linking Preservation *)
-  Lemma preserves_linking f w x e1 e2 e1' e2' :
-    (w \in Exposed) ->
-    (forall w0, w0 \in Exposed -> w0 = w) ->
-    trans_correct e1 e2 ->
-    trans_correct e1' e2' ->
-    trans_correct (A0.link f x e1 e1') (A1.link f w x e2 e2').
-  Proof.
-    unfold A0.link, A1.link.
-    intros.
-    eapply fun_compat; eauto.
-    eapply letapp_compat; eauto.
-  Qed.
+    assert (HV0 : forall i, V i v1 t) by (intros i0; destruct (HV' i0); auto).
+    assert (HV1 : forall i, V i (A0.Vconstr c l) (Tag w (A1.Vconstr c l0))) by (intros i0; destruct (HV' i0); auto).
+    auto.
+  - specialize (H 0); simpl in *.
+    destruct H as [Hw HV].
+    destruct v2; try contradiction; auto.
+    destruct (exposed_reflect w); inv HV.
+    destruct v; try contradiction; auto.
+Qed.
 
-End AnnotateComp.
+Corollary R_res_val_ref {v1 v2} :
+  (forall i, R i (A0.Res v1) (A1.Res v2)) ->
+  val_ref v1 v2.
+Proof. intros; eapply V_val_ref; eauto. Qed.
+
+(* Linking Preservation *)
+(* Note the linking theorem is abstract so that it can work for any analyses. *)
+Theorem preserves_linking f w x e1 e2 e1' e2' :
+  (w \in Exposed) ->
+  (forall w0, w0 \in Exposed -> w0 = w) ->
+  trans_correct e1 e2 ->
+  trans_correct e1' e2' ->
+  trans_correct (A0.link f x e1 e1') (A1.link f w x e2 e2').
+Proof.
+  unfold A0.link, A1.link.
+  intros.
+  eapply fun_compat; eauto.
+  eapply letapp_compat; eauto.
+Qed.
--- src/Framework/W0.v
+++ src/Framework/W0.v
@@ -3,24 +3,28 @@ From CertiCoq.LambdaANF Require Import Ensembles_util map_util set_util List_uti
 From Framework Require Import ANF.
 
 (* Single Exposed Web Id *)
+(* Assume single exposed web id for convienience *)
 
-Module W0.
+Parameter w0 : web.
+Parameter w0_exposed : w0 \in Exposed.
+Parameter Exposed_singleton : forall w, w \in Exposed -> w = w0.
 
-  (* Assume single exposed web id for convienience *)
+Lemma w0_exposedb : exposedb w0 = true.
+Proof.
+  destruct (exposed_reflect w0); auto.
+  exfalso.
+  apply n.
+  apply w0_exposed; auto.
+Qed.
 
-  Parameter w0 : web.
-  Parameter w0_exposed : w0 \in Exposed.
-  Parameter Exposed_singleton : forall w, w \in Exposed -> w = w0.
+Theorem Exposed_nonempty : exists w, w \in Exposed.
+Proof. exists w0. apply w0_exposed. Qed.
 
-  Lemma w0_exposedb : exposedb w0 = true.
-  Proof.
-    destruct (exposed_reflect w0); auto.
-    exfalso.
-    apply n.
-    apply w0_exposed; auto.
-  Qed.
-
-  Theorem Exposed_nonempty : exists w, w \in Exposed.
-  Proof. exists w0. apply w0_exposed. Qed.
-
-End W0.
+Theorem Exposed_unique w :
+  (w \in Exposed) ->
+  forall w0 : web, Ensembles.In web Exposed w0 -> w0 = w.
+Proof.
+  intros.
+  apply Exposed_singleton in H; subst.
+  apply Exposed_singleton in H0; auto.
+Qed.
