5f25b616445e83ad153bac1669b613219679984e
lzheng5 <lingxiaozheng515@gmail.com>
2026-05-18 08:44:25 -0400

    progress

--- src/Framework/CheckingAnnotate2.v
+++ src/Framework/CheckingAnnotate2.v
@@ -13,24 +13,27 @@ Module AT := ANF.
 
 (* Annotate Based on The Checking Semantics *)
 
-Definition web_map : Type := M.t web. (* label -> web *)
-Definition arg_web_map : Type := M.t webs. (* web -> web set *)
-Definition annotate_info : Type := web_map * arg_web_map.
+(* Maps labels to the actual webs. *)
+Definition web_map : Type := M.t web.
+
+(* Maps internal webs, `w`, to webs that interact with. `w` *)
+Definition internal_web_map : Type := M.t webs.
+
+Definition annotate_info : Type := web_map * internal_web_map.
 
 (* Invariants for [annotate_info] *)
-(* 1. Dom of arg_web_map only contains non-exposed web ids *)
-(* 2. Dom of arg_web_map is isomorphic to (Im of web_map \ Exposed) *)
-(* 3. Each set in (Im of arg_web_map) is a subset of (Im of web_map \ Exposed) *)
+(* 1. Dom of internal_web_map only contains non-exposed web ids (encoded) *)
+(* 2. Dom of internal_web_map is isomorphic to (Im of web_map \ Exposed) *)
+(* 3. Each set in (Im of internal_web_map) is a subset of (Im of web_map U Exposed) *)
 
 Definition annotate_info_inv (ainfo : annotate_info) : Prop :=
   match ainfo with
-  | (wm, am) =>
-      (forall w, (w \in Dom_map am) -> ~ (w \in Exposed))
+  | (wm, im) =>
+      (forall w, (w \in Dom_map im) -> ~ (w \in Exposed))
   end.
 
 Section Checking.
 
-  (* The analysis result mapping labels to the actual webs. *)
   Variable W : annotate_info.
 
   Definition to_exposed (tv : AS.wval) : Prop :=
@@ -44,19 +47,20 @@ Section Checking.
     | AS.Res v => to_exposed v
     end.
 
-  Definition record_for (w : web) (tv : AS.wval) : Prop :=
+  (* Records webs that interact with internal web, `w` *)
+  Definition interact_with (w : web) (tv : AS.wval) : Prop :=
     match tv with
     | AS.TAG _ l v =>
-        exists w' am,
+        exists w' im,
           (fst W) ! l = Some w' /\
-          (snd W) ! w = Some am /\
-          (w' \in am)
+          (snd W) ! w = Some im /\
+          (w' \in im)
     end.
 
-  Definition record_res_for (w : web) (r : AS.res) : Prop :=
+  Definition interact_with_res (w : web) (r : AS.res) : Prop :=
     match r with
     | AS.OOT => True
-    | AS.Res v => record_for w v
+    | AS.Res v => interact_with w v
     end.
 
   (* `W` is a valid analysis result with respect to the checking big-step semantics *)
@@ -82,7 +86,7 @@ Section Checking.
       (fst W) ! l' = Some w ->
       (if exposedb w
        then Forall to_exposed vs /\ to_exposed_res r
-       else Forall (record_for w) vs /\ record_res_for w r) ->
+       else Forall (interact_with w) vs /\ interact_with_res w r) ->
       cbstep_fuel (exposedb w) ρ'' e c r ->
       cbstep ex ρ (AS.Eapp f l xs) c r
 
@@ -97,7 +101,7 @@ Section Checking.
       (fst W) ! l' = Some w ->
       (if exposedb w
        then Forall to_exposed vs /\ to_exposed v
-       else Forall (record_for w) vs /\ record_res_for w r) ->
+       else Forall (interact_with w) vs /\ interact_with_res w r) ->
       cbstep ex ρ (AS.Eletapp x f l xs k) (c + c') r
 
   | Cbstep_letapp_OOT :
@@ -110,7 +114,7 @@ Section Checking.
       (fst W) ! l' = Some w ->
       (if exposedb w
        then Forall to_exposed vs
-       else Forall (record_for w) vs) ->
+       else Forall (interact_with w) vs) ->
       cbstep ex ρ (AS.Eletapp x f l xs k) c AS.OOT
 
   | Cbstep_constr :
@@ -119,7 +123,7 @@ Section Checking.
       (fst W) ! l = Some w ->
       (if exposedb w
        then Forall to_exposed vs
-       else Forall (record_for w) vs) ->
+       else Forall (interact_with w) vs) ->
       cbstep_fuel ex (M.set x (AS.Tag l (AS.Vconstr t vs)) ρ) e c r ->
       cbstep ex ρ (AS.Econstr x l t xs e) c r
 
@@ -165,7 +169,46 @@ Section Checking.
     r' = AS.Res v' ->
     (v = v' /\ c = c').
   Proof.
-  Admitted.
+    intros H.
+    generalize dependent v'.
+    generalize dependent r'.
+    generalize dependent c'.
+    generalize dependent v.
+    induction H using cbstep_ind' with (P := fun ex ρ e c r =>
+                                              forall v c' r' v',
+                                                cbstep ex ρ e c' r' ->
+                                                r = AS.Res v -> r' = AS.Res v' ->
+                                                v = v' /\ c = c')
+                                      (P0 := fun ex ρ e c r =>
+                                               forall v c' r' v',
+                                                 cbstep_fuel ex ρ e c' r' ->
+                                                 r = AS.Res v -> r' = AS.Res v' ->
+                                                 v = v' /\ c = c');
+      intros; subst.
+    - inv H1; inv H2; invc; auto.
+    - inv H1.
+      edestruct IHcbstep; eauto.
+    - inv H6; invc.
+      edestruct IHcbstep; eauto.
+    - inv H7; invc.
+      edestruct IHcbstep; eauto.
+      subst.
+      edestruct IHcbstep0; eauto.
+    - inv H7.
+    - inv H3; invc.
+      edestruct IHcbstep; eauto.
+    - inv H4; invc.
+      edestruct IHcbstep; eauto.
+    - inv H4; invc.
+      destruct (find_tag_deterministic H0 H9); subst.
+      edestruct IHcbstep; eauto.
+    - inv H0.
+    - destruct ex; inv H1.
+      + unfold to_exposed_res, to_exposed in *.
+        destruct v; destruct v'.
+        edestruct IHcbstep; eauto.
+      + edestruct IHcbstep; eauto.
+  Qed.
 
   Theorem cbstep_fuel_deterministic v v' {ex ρ e c c' r r'}:
     cbstep_fuel ex ρ e c r ->
@@ -234,19 +277,62 @@ End Checking.
 Section Approx.
 
   (* W1 \leq W2 iff W2 over-approximates W1 and annotate_info_inv are preserved *)
-  (* We describe over-approximation via a web -> web function *)
+  (* We describe over-approximation via a web -> web mapping *)
   Definition leq (W1 W2 : annotate_info) :=
     exists f : web -> web,
       (forall w, w \in Exposed -> f w \in Exposed) /\
       (forall l w, (fst W1) ! l = Some w -> (fst W2) ! l = Some (f w)) /\
-      (forall l w am,
-          (fst W1) ! l = Some w ->
-          (snd W1) ! w = Some am ->
-          (~ w \in Exposed) ->
-          ((f w) \in Exposed) ->
-          ((forall w',
-             (w' \in am) ->
-             (f w' \in Exposed)) /\
-           (snd W2) ! (f w) = None)).
+      (* (snd W1) -> (snd W2) *)
+      (forall w im,
+          (snd W1) ! w = Some im ->
+          (if exposedb (f w)
+           then ((snd W2) ! (f w) = None /\
+                 (forall w', (w' \in im) -> (f w' \in Exposed)))
+           else exists im',
+                  (snd W2) ! (f w) = Some im' /\
+                  (* im' ⊆ f(im) *)
+                  (forall w', (w' \in im) -> f w' \in im'))) /\
+        (* (snd W1) <- (snd W1) *)
+        (forall w',
+            w' \in Dom_map (snd W2) ->
+            exists w,
+              w \in Dom_map (snd W1) /\ f w = w').
+
+  Lemma leq_refl W :
+    annotate_info_inv W ->
+    leq W W.
+  Proof.
+    destruct W as [wm im]. simpl. intros Hinv.
+    unfold leq; simpl.
+    exists (fun x => x).
+    split; [auto|].
+    split; [auto|].
+    split.
+    - intros w im0 Hsnd.
+      destruct (exposed_reflect w) as [Hexp | Hnexp].
+      + exfalso. apply (Hinv w); [eexists; eauto | auto].
+      + eexists; split; eauto.
+    - intros w' [im0 Hget].
+      exists w'; split; [eexists; eauto | auto].
+  Qed.
+
+  Lemma leq_preserves_annotate_info_inv W1 W2:
+    leq W1 W2 ->
+    annotate_info_inv W1 ->
+    annotate_info_inv W2.
+  Proof.
+    destruct W1 as [wm1 im1]. destruct W2 as [wm2 im2]. simpl.
+    intros [f [Hexp [Hfst [Hsnd Hsurj]]]] Hinv1.
+    intros w Hdom Hexpw.
+    destruct (Hsurj w Hdom) as [w0 [[im0 Hsnd0] Hfw0]].
+    specialize (Hsnd w0 im0 Hsnd0).
+    subst w.
+    destruct (exposed_reflect (f w0)) as [Hexpfw | Hnexpfw].
+    - destruct Hsnd as [Hnone _].
+      destruct Hdom as [im2v Hdomw].
+      simpl in *.
+      rewrite Hnone in Hdomw. discriminate.
+    - apply Hnexpfw; exact Hexpw.
+  Qed.
 
 End Approx.
